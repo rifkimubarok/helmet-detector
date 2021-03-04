36951c8a6a87126bdc4e1baec857ca4e26dd209b
Rifki Mubarok <177006078@student.unsil.ac.id>
2021-03-04 11:51:37 +0700

    ok

--- training.py
+++ training.py
@@ -3,5 +3,5 @@ from imageai.Detection.Custom import DetectionModelTrainer
 trainer = DetectionModelTrainer()
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory="helmet")
-trainer.setTrainConfig(object_names_array=["With Helmet", "Without Helmet"], batch_size=4, num_experiments=100, train_from_pretrained_model="pretrained-yolov3.h5")
+trainer.setTrainConfig(object_names_array=["With Helmet", "Without Helmet"], batch_size=1, num_experiments=2, train_from_pretrained_model="pretrained-yolov3.h5")
 trainer.trainModel()
